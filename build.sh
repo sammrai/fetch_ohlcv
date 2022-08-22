98f054acd665e062f9ca0659d562b4b7b95c0931
saku <saku0aab@gmail.com>
2022-08-22 22:09:08 +0900

    ビルドし直し

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+
+function build_image(){
+    imgname=$1
+    docker build --tag=$imgname .
+    docker push $imgname
+}
+
+if [ "$1" = "test" ];then
+  imgname=sammrai/fetch_ohlcv:test
+  docker build --tag=$imgname .
+  docker run --rm $imgname --exchanges ftx kraken --symbols BTC/USD ETH/USD
+  exit
+fi
+
+
 if [ -z "$(git status --porcelain)" ]; then
   echo "start builds."
 else
@@ -7,12 +22,6 @@ else
   echo "commit first before build."
   exit 1
 fi
-
-function build_image(){
-    imgname=$1
-    docker build --tag=$imgname .
-    docker push $imgname
-}
 
 tag=`git rev-parse --short HEAD`
 build_image sammrai/fetch_ohlcv:$tag
